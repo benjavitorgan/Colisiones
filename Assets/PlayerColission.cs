7f0b870becc57c7d677856e969db9ec839b38fbb
benjavitorgan <vitorganbenjamin@gmail.com>
2022-05-24 15:58:57 -0300

    Collision lesson and mini game finished

---
+++ Assets/PlayerColission.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColission : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            transform.position = new Vector3 (-3,0.5f,-3);
+        }
+        
+    }
+
+    void OnCollisionExit (Collision col)
+    {
+        if (col.gameObject.name == "Plane")
+        {
+            transform.position = new Vector3(-3, 0.5f, -3);
+        }
+    }
+}
